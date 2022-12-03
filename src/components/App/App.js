ae99302430e848f96d83e0f6549a705c4f3f6027
Norbs <97234527+norbertcsipak@users.noreply.github.com>
2022-12-03 16:42:37 +0000

    created search bar components and learnt how to add individual components to App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,18 @@
+import React from 'react';
 import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import BusinessList from '../BusinessList/BusinessList';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div class="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,2 +1,39 @@
 import React from 'react';
 import './SearchBar.css';
+
+const sortByOptions = {
+  'Best Match': 'best_match',
+  'Highest Rated': 'rating',
+  'Most Viewed': 'review_count'
+};
+
+class SearchBar extends React.Component {
+  renderSortByOptions() {
+    // returns all keys from the object
+    return Object.keys(sortByOptions).map(sortByOption => {
+      let sortByOptionValue = sortByOptions[sortByOption];
+      return <li key={sortByOptionValue}>{sortByOption}</li>
+    })
+  }
+
+  render() {
+    return (
+      <div className="SearchBar">
+        <div className="SearchBar-sort-options">
+          <ul>
+            {/* calling SortByOption method that refers to its class use this */}
+            {this.renderSortByOptions()}
+          </ul>
+        </div>
+        <div className="SearchBar-fields">
+          <input placeholder="Search Businesses" />
+          <input placeholder="Where?" />
+        </div>
+        <div className="SearchBar-submit">
+          <a href='www.#.com'>Let's Go</a>
+        </div>
+      </div>
+    )
+  }
+}
+export default SearchBar;
